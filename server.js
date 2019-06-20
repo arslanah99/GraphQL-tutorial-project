6c2e8dd9110e51f9f228a13740416fce61339004
arslan <arah09@hotmail.com>
2019-06-20 13:27:49 -0600

    created boilerplate code for the server.js file

---
+++ server.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+const port = 9000;
+
+const app = express();
+app.use(cors(), bodyParser.json())
+app.listen(port, () => console.log(`Server running on port ${port}`));
