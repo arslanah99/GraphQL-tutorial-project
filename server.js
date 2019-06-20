ed671a822e3f4c1df8bcaef2f898d942b4deb336
arslan <arah09@hotmail.com>
2019-06-20 13:59:42 -0600

    fixed error where i was using graphql apollo

--- server.js
+++ server.js
@@ -1,9 +1,25 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const cors = require('cors');
+const graphqlExpress = require('express-graphql');
 
-const port = 9000;
+const bodyParser = require('body-parser')
+const cors = require('cors')
+const express = require('express')
+const { makeExecutableSchema } = require('graphql-tools')
 
-const app = express();
-app.use(cors(), bodyParser.json());
-app.listen(port, () => console.log(`Server running on port ${port}`));
+const port = 9000
+
+const typeDefs = `
+    type Query {
+        greeting: String
+    }
+`
+const resolvers = {
+    Query: {
+        greeting: () => `Hello World!`
+    }
+}
+
+const schema = makeExecutableSchema({ typeDefs, resolvers })
+const app = express()
+app.use(cors(), bodyParser.json())
+app.use('/graphql', graphqlExpress({ schema, graphiql: true }))
+app.listen(port, () => console.log(`server running on port ${port}`))
